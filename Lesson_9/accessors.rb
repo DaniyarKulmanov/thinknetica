75a2ae4c30d92c56abb42638a181d3ad6dfd9d7e
sindry <DanRubyDeveloper@gmail.com>
2017-09-06 16:31:06 +0600

    Fix 9.1 recommendations

--- Lesson_9/accessors.rb
+++ Lesson_9/accessors.rb
@@ -1,7 +1,6 @@
 # Accessors module
 module Accessors
   def self.included(base)
-    base.include InstanceMethods
     base.extend ClassMethods
   end
 
@@ -10,16 +9,15 @@ module Accessors
     def attr_accessor_with_history(*methods)
       options = methods.last.is_a?(Hash) ? methods.pop : {}
       methods.each do |method|
-        method.to_sym unless method.is_a? Symbol
         define_method "#{method}_history" do
-          instance_variable_get("@#{method}_history") || [nil]
+          instance_variable_get("@#{method}_history") || []
         end
         define_method(method) do
           instance_variable_get("@#{method}") || instance_variable_set("@#{method}", options[:default])
         end
         define_method("#{method}=") do |v|
           value = instance_variable_get("@#{method}_history")
-          value ||= [nil]
+          value ||= []
           value << v
           instance_variable_set("@#{method}", v)
           instance_variable_set("@#{method}_history", value)
@@ -30,23 +28,15 @@ module Accessors
     def strong_attr_accessor(*methods)
       options = methods.last.is_a?(Hash) ? methods.pop : {}
       methods.each do |method|
-        method.to_sym unless method.is_a? Symbol
         define_method(method) do
           instance_variable_get("@#{method}") || instance_variable_set("@#{method}", options[:default])
         end
         define_method("#{method}=") do |v|
           message = "input value class must be #{options[:class]} "
-          raise TypeError.new, message if v.class.to_s != options[:class].to_s
+          raise TypeError.new, message unless v.is_a? options[:class]
           instance_variable_set("@#{method}", v)
         end
       end
     end
-  end
-
-  # instance methods
-  module InstanceMethods
-    # ...
-    # attribute_history
-    # strong_attr_accessor
   end
 end
